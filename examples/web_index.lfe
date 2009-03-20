a990256d467421ba77471e0eefef2f11cd97429d
cadar <cadar777@gmail.com>
2009-03-20 11:39:40 +0100

    Makefile now compile multiple lfe files.

---
+++ examples/web_index.lfe
@@ -0,0 +1,13 @@
+(define-module web_index
+  (export all))
+
+
+(include-file "lfeweb/wf.lfe")
+
+(defun main () (make-template file '"./wwwroot/template.html"))
+
+(defun title () '"LFEweb")
+(defun body () (make-label text '"First LFEweb page"))
+
+(defun event (_) 'ok)
+
--- include.mk
+++ include.mk
@@ -1,23 +1,43 @@
+vpath %.lfe  ./src/pages
+vpath %.beam ./ebin
 
-PAGES=src/pages/
-MAIN=web_blog1
+LSRCS=web_index.lfe web_blog1.lfe
+
+LOBJS=$(LSRCS:.lfe=.beam)
 LFE_EBIN=${HOME}/lfe/ebin/
 
-all: compile ebin/web_blog1.beam
+ERL_LOAD='code:load_file(lfe_comp).'
+ERL_COMP='File=hd(init:get_plain_arguments()), try lfe_comp:file(File) of {ok,_Module} -> halt(0); error -> halt(0); All ->  io:format("./~s:1: ~p~n",[File,All]) catch X:Y -> io:format("./~s:1: Catch outside of compiler: ~p ~p ~n",[File,X,Y]) end, halt(0).'
 
-ebin/web_blog1.beam:
-	@erl -noshell -pa ${LFE_EBIN} -eval 'code:load_file(lfe_comp).' -eval 'lfe_comp:file(hd(init:get_plain_arguments())), halt(0).' -extra ${PAGES}${MAIN}.lfe
-	mv web_blog1.beam ebin
+.PHONY: all
+
+all: compile $(LOBJS)
+
+%.beam : %.lfe
+	@echo Recompile: $<
+	@erl -noshell -pa $(LFE_EBIN) -eval $(ERL_LOAD) -eval $(ERL_COMP) -extra $< 
+	@mv $@ ebin 
+
+lclean: clean
+	rm -rf compile.err compile.out *.dump 
+
+wipe: clean lclean
+	rm -rf *.beam
 
 
+
+FLY_BEAM=$(notdir $(CHK_SOURCES:.lfe=.beam))
+BEAM=$(notdir $(CHK_SOURCES:_flymake.lfe=.beam)) 
+MODULE=$(notdir $(CHK_SOURCES:_flymake.lfe=)) 
+LOG=2> compile.err | tee compile.out
 
 check-syntax:
-	@erl -noshell -pa ${LFE_EBIN} \
-	-eval 'code:load_file(lfe_comp).' \
-	-eval 'File=hd(init:get_plain_arguments()), try lfe_comp:file(File) of {ok,_Module} -> halt(0); error -> halt(0); All ->  io:format("./~s:1: ~p~n",[File,All]) catch X:Y -> io:format("./~s:1: Catch outside of compiler: ~p ~p ~n",[File,X,Y]) end, halt(0).' \
-	-extra ${PAGES}${MAIN}_flymake.lfe 2> compile.err | tee compile.out
-	rm ${MAIN}_flymake.beam
-#	@screen -p server -X stuff $''sync:go().'
+	@erl -noshell -pa ${LFE_EBIN} -eval $(ERL_LOAD) -eval $(ERL_COMP) -extra  $(CHK_SOURCES) $(LOG)
+	mv $(FLY_BEAM) ebin/$(BEAM)  $(LOG)
+	@screen -p server -X stuff $''code:purge($(MODULE)),code:load_file($(MODULE)).'
+	echo BrowserReload\(\)\; repl.quit\(\) | nc localhost 4242
+
+
 
 help:
 	@echo ";; Copy to .emacs, then restart."
@@ -26,4 +46,15 @@ help:
 	@echo "  (add-hook 'find-file-hook 'flymake-find-file-hook)"
 	@echo "  (add-to-list 'flymake-allowed-file-name-masks"
 	@echo "	       '(\"\\\\\.lfe\\\\\'\" flymake-simple-make-init)))"
+	@echo ""
+	@echo "(autoload 'moz-minor-mode \"moz\" \"Mozilla Minor and Inferior Mozilla Modes\" t)"
+	@echo "(add-hook 'javascript-mode-hook 'javascript-custom-setup)"
+	@echo "    (defun javascript-custom-setup ()"
+	@echo "      (moz-minor-mode 1))"
+	@echo "(global-set-key (kbd \"C-x p\")"
+	@echo "                (lambda ()"
+	@echo "                  (interactive)"
+	@echo "                  (comint-send-string (inferior-moz-process)"
+	@echo "                                      \"BrowserReload();\")))"
+
 
